857b0f1aafe60c0d010ec56e783c6ce687f0c8a6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-01-22 20:26:21 +0100

    Collection all master IPs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "swarm_ip" {
   value = "${digitalocean_droplet.docker_swarm_master_initial.ipv4_address}"
 }
 
+# All master IPs
+output "swarm_ips" {
+  value = ["${digitalocean_droplet.docker_swarm_master.*.ipv4_address}"]
+}
+
 output "swarm_user" {
   value = "${var.do_user}"
 }
